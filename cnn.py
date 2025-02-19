4160c047cca34e0e699f40ddbbf03c95dbd100f9
k-reddy <reddypkeerthi@gmail.com>
2025-02-18 17:54:57 -0800

    adding bottleneck blocks and max pool layers to speed up the model while keeping stride 1 for early layers

--- cnn.py
+++ cnn.py
@@ -52,6 +52,66 @@ class ResBlock(nn.Module):
         return out
 
 
+class BottleneckResBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, dilation=1, reduction=4):
+        super().__init__()
+        # Reduce channels by reduction factor
+        bottleneck_channels = in_channels // reduction
+
+        # Bottleneck path
+        self.conv1 = nn.Conv2d(
+            in_channels, bottleneck_channels, kernel_size=1, bias=False
+        )
+        self.bn1 = nn.BatchNorm2d(bottleneck_channels)
+
+        self.conv2 = nn.Conv2d(
+            bottleneck_channels,
+            bottleneck_channels,
+            kernel_size=3,
+            stride=stride,
+            padding=dilation,
+            dilation=dilation,
+            bias=False,
+        )
+        self.bn2 = nn.BatchNorm2d(bottleneck_channels)
+
+        self.conv3 = nn.Conv2d(
+            bottleneck_channels, out_channels, kernel_size=1, bias=False
+        )
+        self.bn3 = nn.BatchNorm2d(out_channels)
+
+        # Shortcut connection
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(
+                    in_channels, out_channels, kernel_size=1, stride=stride, bias=False
+                ),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        self.relu = nn.ReLU(inplace=False)
+
+    def forward(self, x):
+        residual = self.shortcut(x)
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        out = self.relu(out)
+
+        out = self.conv3(out)
+        out = self.bn3(out)
+
+        out = out + residual
+        out = self.relu(out)
+
+        return out
+
+
 class MushroomClassifier(nn.Module):
     """
     Defines a CNN used to classify mushrooms
@@ -63,13 +123,23 @@ class MushroomClassifier(nn.Module):
         super().__init__()
 
         self.initial = nn.Sequential(
-            ConvBlock(3, 32, stride=1), ConvBlock(32, 32, stride=1)
+            ConvBlock(3, 16, stride=1),
+            ConvBlock(16, 32, stride=1),
+            nn.MaxPool2d(kernel_size=2, stride=2),
         )
 
-        self.block1 = ResBlock(32, 64, stride=1)
-        self.block2 = ResBlock(64, 128, stride=2)
+        self.block1 = nn.Sequential(
+            BottleneckResBlock(32, 64), nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+        self.block2 = nn.Sequential(
+            BottleneckResBlock(64, 128), nn.MaxPool2d(kernel_size=2, stride=2)
+        )
         self.dropout1 = nn.Dropout(0.2)
-        self.block3 = ResBlock(128, 256, stride=2, dilation=2)
+        self.block3 = self.block2 = nn.Sequential(
+            BottleneckResBlock(128, 256, dilation=2),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+        )
         self.dropout2 = nn.Dropout(0.2)
 
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
